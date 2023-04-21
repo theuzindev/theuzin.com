fc659a8717fc8d2a1fb0ed69471c30397dc9ebd3
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-04-21 14:49:43 -0300

    improve scroping books

--- app/Console/Commands/ScrapeBooks.php
+++ app/Console/Commands/ScrapeBooks.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Book;
 use App\Support\HasClient;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 use PHPHtmlParser\Dom;
 use PHPHtmlParser\Dom\HtmlNode;
 
@@ -28,5 +29,11 @@ class ScrapeBooks extends Command
             ->each(function (HtmlNode $node) {
                 Book::query()->firstOrCreate(['content' => $node->text]);
             });
+
+        $books = Book::all(['content'])->toJson(
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+        );
+
+        Storage::put('users.json', $books);
     }
 }
