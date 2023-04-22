163d95b94ddde82ab1b4405ad0bdc4f76ef35c5b
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-04-21 22:09:49 -0300

    finish daily recommendaitons

--- app/Http/Livewire/DailyRecommendations.php
+++ app/Http/Livewire/DailyRecommendations.php
@@ -2,31 +2,55 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 use Livewire\Component;
 use OpenAI\Laravel\Facades\OpenAI;
 
 class DailyRecommendations extends Component
 {
+    public string $selected = '';
+
     public ?string $result = null;
+
+    public function mount()
+    {
+        $ip = getenv('REMOTE_ADDR');
+
+        if (cache()->has('selected::' . $ip)) {
+            $this->selected = cache('selected::' . $ip);
+        }
+
+        if (cache()->has($this->selected . '::' . $ip)) {
+            $this->result = cache($this->selected . '::' . $ip);
+        }
+    }
 
     public function food(): void
     {
+        $this->selected = __FUNCTION__;
+
         $this->result = $this->askGPT('Recommend me a single food to order right now');
     }
 
     public function style(): void
     {
+        $this->selected = __FUNCTION__;
+
         $this->result = $this->askGPT('Recommend me a single clothing style to wear for today');
     }
 
     public function book(): void
     {
+        $this->selected = __FUNCTION__;
+
         $this->result = $this->askGPT('Recommend me a single book to read today');
     }
 
     public function game(): void
     {
+        $this->selected = __FUNCTION__;
+
         $this->result = $this->askGPT('Recommend me a single video game to play right now');
     }
 
@@ -35,22 +59,32 @@ class DailyRecommendations extends Component
         return view('livewire.daily-recommendations');
     }
 
-    private function askGPT(string $content): string
+    private function askGPT(string $message): string
     {
-        $result = OpenAI::completions()->create([
-            'model' => 'gpt-3.5-turbo',
-            'messages' => [
-                [
-                    'role' => 'assistant',
-                    'content' => $content,
-                ],
-                // [
-                //     'role' => 'user',
-                //     'content' => 'Hello!'
-                // ]
-            ],
-        ]);
+        $ip = getenv('REMOTE_ADDR');
 
-        return $result['choices'][0]['text'];
+        Cache::put('selected::' . $ip, $this->selected, now()->addDay());
+
+        return Cache::remember(
+            $this->selected . '::' . $ip,
+            now()->addDay(),
+            function () use ($message) {
+                $result = OpenAI::chat()->create([
+                    'model' => 'gpt-3.5-turbo',
+                    'messages' => [
+                        [
+                            'role' => 'assistant',
+                            'content' => 'Sorry, my role is only to give daily recommendation of food, clothing style, video game or book',
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $message,
+                        ]
+                    ],
+                ]);
+
+                return $result['choices'][0]['message']['content'];
+            }
+        );
     }
 }
--- resources/views/livewire/daily-recommendations.blade.php
+++ resources/views/livewire/daily-recommendations.blade.php
@@ -2,7 +2,7 @@
     <h1 class="font-bangers text-5xl text-center">Today`s Recommendations</h1>
 
     <div class="flex justify-evenly items-center mt-14">
-        <button wire:click="food" class="bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-10 shadow-md shadow-red-200">
+        <button wire:click="food" class="{{ $selected == 'food' ? 'bg-gradient-to-tr from-gray-300 to-gray-100' : '' }} bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-10 shadow-md shadow-red-200">
             <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pizza">
                 <path d="M15 11h.01"></path>
                 <path d="M11 15h.01"></path>
@@ -12,13 +12,13 @@
             </svg>
         </button>
 
-        <button wire:click="style" class="bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-7 shadow-md shadow-yellow-200">
+        <button wire:click="style" class="{{ $selected == 'style' ? 'bg-gradient-to-tr from-gray-300 to-gray-100' : '' }} bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-7 shadow-md shadow-yellow-200">
             <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shirt">
                 <path d="M20.38 3.46 16 2a4 4 0 0 1-8 0L3.62 3.46a2 2 0 0 0-1.34 2.23l.58 3.47a1 1 0 0 0 .99.84H6v10c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V10h2.15a1 1 0 0 0 .99-.84l.58-3.47a2 2 0 0 0-1.34-2.23z"></path>
             </svg>
         </button>
 
-        <button wire:click="game" class="bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-7 shadow-md shadow-blue-200">
+        <button wire:click="game" class="{{ $selected == 'game' ? 'bg-gradient-to-tr from-gray-300 to-gray-100' : '' }} bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-7 shadow-md shadow-blue-200">
             <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-gamepad-2">
                 <line x1="6" x2="10" y1="11" y2="11"></line>
                 <line x1="8" x2="8" y1="9" y2="13"></line>
@@ -28,7 +28,7 @@
             </svg>
         </button>
 
-        <button wire:click="book" class="bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-7 shadow-md shadow-green-200">
+        <button wire:click="book" class="{{ $selected == 'book' ? 'bg-gradient-to-tr from-gray-300 to-gray-100' : '' }} bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-7 shadow-md shadow-green-200">
             <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book">
                 <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
                 <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
@@ -42,9 +42,13 @@
         x-transition.duration.1000ms
         x-cloak
     >
-        <div wire:loading class="p-10 bg-gray-200 animate-pulse rounded-lg text-center text-xl">Loading...</div>
-        <div wire:loading.remove class="bg-white flex w-full p-14 text-2xl rounded-lg">
-            {{ $result }}
+        <div wire:loading class="p-10 bg-gray-200 w-full animate-pulse rounded-lg text-center text-xl">Loading...</div>
+        <div wire:loading.remove class="bg-white w-full p-14 text-2xl rounded-lg">
+            <h2 class="text-4xl capitalize font-semibold pb-5 border-b">
+                {{ $selected }} <span class="text-xl text-gray-600 ml-3">({{ date('Y-m-d') }})</span>
+            </h2>
+
+            <p class="mt-5">{{ $result }}</p>
         </div>
     </div>
 </div>
