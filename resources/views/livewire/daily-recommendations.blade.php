e4aa29275fae6d56279914e8a9ad9df837758d1f
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-05-02 16:41:14 -0300

    add daily recommendations head card

--- resources/views/livewire/daily-recommendations.blade.php
+++ resources/views/livewire/daily-recommendations.blade.php
@@ -1,7 +1,14 @@
 <div x-data="{ hasResult: @entangle('result') }">
-    <h1 class="font-bangers text-5xl text-center">Today`s Recommendations</h1>
+    <h1 class="font-bangers text-5xl text-center">Daily Recommendations</h1>
 
-    <div class="flex justify-evenly items-center mt-14 gap-10 flex-wrap">
+    <div class="bg-sky-100 p-6 rounded-lg mt-10 whitespace-pre-line">Are you unsure of what food to order, what book to read, or which video game to play?
+
+        Look no further than <strong>Daily Recommendations!</strong>
+
+        Our platform provides daily recommendations for food, video games, books, and clothing styles. These recommendations are not just based on simple algorithms, but on Artificial Intelligence that responds to your questions in natural language.
+    </div>
+
+    <div class="flex justify-evenly items-center mt-10 gap-10 flex-wrap">
         <button wire:click="food" class="{{ $selected == 'food' ? 'bg-gradient-to-tr from-gray-300 to-gray-100' : '' }} bg-white hover:animate-pulse hover:bg-gray-50 hover:scale-110 hover:transition-all rounded-full p-10 shadow-md shadow-red-200">
             <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pizza">
                 <path d="M15 11h.01"></path>
