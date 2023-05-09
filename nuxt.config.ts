c95c19b8b0717063f3cac8f6bb3d3dcef238348e
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-05-09 04:01:42 -0300

    modify favicon and assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.png",
+        },
+      ],
+    },
+  },
+});
